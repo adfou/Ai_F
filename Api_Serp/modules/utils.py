1b1ed19c1b39a454c327cb0e091d67aa29382d5e
fouad berriche <adfouberr@gmail.Com>
2023-04-29 18:27:47 +0100

    test

--- Api_Serp/modules/utils.py
+++ Api_Serp/modules/utils.py
@@ -55,7 +55,8 @@ def Lemmatizer_stop_word(sentence):
 # Stop word removal
 def remove_stopwords(phrase):
     """Function to remove stopwords"""
-    stop_words = stopwords.words('english')
+    stop_words = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 
+'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
     words = word_tokenize(phrase)
     # Stopwords removing
     stripped_phrase = []
@@ -217,7 +218,6 @@ def make_prediction(arabic_sentence, model_path, label_encoder_path):
     # Return the result
     return y_ar, y_en, class_op_ar, class_op_en
 
-##############################################################
 
 
 
--- Api_Serp/test.py
+++ Api_Serp/test.py
@@ -1,19 +1,4 @@
-from datetime import datetime
-def diffrence_time(time,time_report):
-    datetime_news = datetime.strptime(time, '%Y-%m-%d %H:%M:%S.%f')
-    datetime_report = datetime.strptime(time_report, '%Y-%m-%d %H:%M:%S.%f')
-
-    timedelta = datetime_news - datetime_report
-    return timedelta
-time = '2023-04-29T13:28:14.990189'
-
-
-time = time.replace('T', ' ')
-
-time_report = '23-04-23 05:07:10.447200'
-time_report = '20'+time_report
-
-print(time)
-
-
-print(diffrence_time(time,time_report) )
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+print(word_tokenize("انا حزين"))
